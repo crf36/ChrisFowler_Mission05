2fc64c42f8834d805bbe3aba39298ac11d40d1d2
crf36 <149890484+crf36@users.noreply.github.com>
2024-02-07 08:00:08 -0700

    Add project files.

---
+++ ChrisFowler_Mission05/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using ChrisFowler_Mission05.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ChrisFowler_Mission05.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChrisFowler_Mission05/wwwroot/js/calculate.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#buttonCalculator").click(function () {
+        let hourlyRate = parseFloat($("#hourlyRate").val());
+        let hours = parseFloat($("#numHours").val());
+
+        if(isNaN(hours) || hours < 0)
+        {
+            $("#error").text("Please enter a valid positive number");
+            return;
+        }
+
+        $("#error").text("");
+
+        let monthlyCost = hours * hourlyRate;
+        $("#monthlyCost").val("$" + monthlyCost.toFixed(2));
+
+    })
+})
